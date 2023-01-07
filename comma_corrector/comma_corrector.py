7e3e69eaeefadd88deb782b81b0b35db9ae008f6
ack2406 <ack9222@gmail.com>
2023-01-07 17:38:35 +0100

    add comments

--- comma_corrector/comma_corrector.py
+++ comma_corrector/comma_corrector.py
@@ -12,8 +12,8 @@ class CommaCorrector:
     def __init__(self, sentence: str = "") -> None:
         self._nlp: Language = load("pl_core_news_sm")
 
-        self._sentence: str = sentence
-        self._sentence_doc: Language = self.create_doc(self._sentence)
+        self.__sentence: str = sentence
+        self.__sentence_doc: Language = self.create_doc(self.__sentence)
 
         # get all rules from rules.py
         self._rules: dict = {rule[6:]: getattr(rules, rule) for rule in dir(
@@ -24,52 +24,87 @@ class CommaCorrector:
 
     @property
     def sentence(self) -> str:
-        return self._sentence
+        return self.__sentence
 
     @sentence.setter
     def sentence(self, sentence: str) -> None:
-        self._sentence = sentence
-        self._sentence_doc = self.create_doc(self._sentence)
+        self.__sentence = sentence
+
+    def create_doc(self, sentence: str) -> Language:
+        """Creates spaCy document from given sentence."""
+        return self._nlp(sentence)
+    
+    def create_docs(self, sentences: list[str]) -> list[Language]:
+        """Creates spaCy documents from given sentences."""
+        return self._nlp.pipe(sentences)
 
     @property
     def sentence_doc(self) -> Language:
-        return self._sentence_doc
+        return self.__sentence_doc
 
-    def _join_sentence(self, sentence: list[str]) -> str:
+    @sentence_doc.setter
+    def sentence_doc(self, sentence_doc: Language) -> None:
+        self.__sentence_doc = sentence_doc
+
+
+    @staticmethod
+    def _join_sentence(sentence: list[str]) -> str:
+        """Joins list of words into sentence. Removes spaces before punctuation marks."""
         sentence = ' '.join(sentence)
 
         sentence = re.sub(r' ([.,?!:;])', r'\1', sentence)
 
         return sentence
 
-    def create_doc(self, sentence: str) -> Language:
-        return self._nlp(sentence)
-
     def correct(self) -> str:
-        sentence_text: list[str] = [token.text for token in self._sentence_doc]
+        """
+        Corrects punctuation in given sentence.
+
+        Returns:
+            str: Corrected sentence.
+        """
+
+        # transform spaCy document into list of words
+        sentence_text: list[str] = [
+            token.text for token in self.__sentence_doc]
+        
+        # shift is used to correct index of token in sentence, if comma was inserted
         shift: int = 0
 
+        # occured is used to check if rule was already used. Used to check for enumeration.
         occured: dict[str, bool] = {key: False for key in self._rules_keys}
 
-        for token in self._sentence_doc:
+        # iterate over all tokens in sentence
+        for token in self.__sentence_doc:
             index = token.i
 
+            # skip first token, because it will never have a comma before it
             if index == 0:
                 continue
 
+            # skip if token is not in rules, because it is (probably) not a word, that can have comma before it
             if token.text not in self._rules_keys:
                 continue
-
+            
+            # get result of rule check in format {'insert': bool, 'insert_pos': int, 'occured': bool}
             result = self._rules[token.text](
-                token, self._sentence_doc[index - 1], occured[token.text])
+                token, self.__sentence_doc[index - 1], occured[token.text])
 
+            # set occured to True, if rule was used and it's possible to be an enumeration
             occured[token.text] = result['occured']
 
+            # skip if comma should not be inserted
             if not result['insert']:
                 continue
 
+            # check if token at position index + result['insert_pos'] is not a punctuation mark
+            if sentence_text[index + result['insert_pos'] + shift - 1] == ',':
+                continue
+
+            # insert comma result['insert_pos'] characters before token
             sentence_text.insert(index + result['insert_pos'], ',')
 
+            # increase shift by 1, because comma was inserted
             shift += 1
 
         return self._join_sentence(sentence_text)
--- comma_corrector/comma_tester.py
+++ comma_corrector/comma_tester.py
@@ -23,36 +23,56 @@ class CommaTester:
         self._data = data
 
     def _rate(self, corrected_sentence: str) -> dict[str, int]:
+        """
+        Rates correctness of corrected sentence.
+        """
+
+        # create spaCy documents
         data_doc: Language = self.corrector.create_doc(self._data)
         corrected_doc: Language = self.corrector.create_doc(corrected_sentence)
 
-
+        # create index for both documents
         data_pos: int = 0
         corrected_pos: int = 0
 
+        # create dict for points
         points: dir[str, int] = {"correct": 0, "incorrect": 0, "missing": 0}
 
+        # iterate through both documents
         while data_pos < len(data_doc) and corrected_pos < len(corrected_doc):
+            # get tokens
             data_token: tokens.token.Token = data_doc[data_pos]
             corrected_token: tokens.token.Token = corrected_doc[corrected_pos]
 
+            # if both tokens are commas, add point to correct
             if data_token.text == ',' and corrected_token.text == ',':
                 points["correct"] += 1
+            # if token in data is comma and token in corrected is not, add point to missing
             elif data_token.text == ',' and corrected_token.text != ',':
                 points["missing"] += 1
                 corrected_pos -= 1
+            # if token in data is not comma and token in corrected is, add point to incorrect
             elif data_token.text != ',' and corrected_token.text == ',':
                 points["incorrect"] += 1
                 data_pos -= 1
 
+            # increment both indexes
             data_pos += 1
             corrected_pos += 1
-        
+
         return points
 
     def test(self) -> dict[str, int]:
+        # transform sentence to sentence without commas for testing purposes
         cleared_sentence = self._data.replace(",", "")
+
+        # set sentence and sentence_doc in corrector
         self.corrector.sentence = cleared_sentence
+        self.corrector.sentence_doc = self.corrector.create_doc(
+            cleared_sentence)
+
+        # correct sentence and get result
         result: str = self.corrector.correct()
 
+        # return rated result in format {'correct': int, 'incorrect': int, 'missing': int}
         return self._rate(result)
