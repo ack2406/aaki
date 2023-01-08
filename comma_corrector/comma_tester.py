04d372baf53746c0167fe6651f5daf0e32e9020d
ack2406 <ack9222@gmail.com>
2023-01-08 02:04:35 +0100

    merge tester files into one

--- comma_corrector/comma_tester.py
+++ comma_corrector/comma_tester.py
@@ -10,69 +10,82 @@ class CommaTester:
     and checks if checker returns correct result.
     """
 
-    def __init__(self):
+    def __init__(self, sentences: list[str] = []):
         self._corrector: CommaCorrector = CommaCorrector()
-        self._data: str = ""
+        self._sentences: list[str] = sentences
 
     @property
-    def data(self) -> str:
-        return self._data
+    def sentences(self) -> str:
+        return self._sentences
 
-    @data.setter
-    def data(self, data: str) -> None:
-        self._data = data
+    @sentences.setter
+    def sentences(self, sentences: str) -> None:
+        self._sentences = sentences
 
-    def _rate(self, corrected_doc: Language, data_doc: Language) -> dict[str, int]:
+    def _rate_sentence(self, corrected_doc: Language, sentences_doc: Language) -> dict[str, int]:
         """
         Rates correctness of corrected sentence.
         """
 
         # create index for both documents
-        data_pos: int = 0
+        sentences_pos: int = 0
         corrected_pos: int = 0
 
         # create dict for points
         points: dir[str, int] = {"correct": 0, "incorrect": 0, "missing": 0}
 
         # iterate through both documents
-        while data_pos < len(data_doc) and corrected_pos < len(corrected_doc):
+        while sentences_pos < len(sentences_doc) and corrected_pos < len(corrected_doc):
             # get tokens
-            data_token: tokens.token.Token = data_doc[data_pos]
+            sentences_token: tokens.token.Token = sentences_doc[sentences_pos]
             corrected_token: tokens.token.Token = corrected_doc[corrected_pos]
 
             # if both tokens are commas, add point to correct
-            if data_token.text == ',' and corrected_token.text == ',':
+            if sentences_token.text == ',' and corrected_token.text == ',':
                 points["correct"] += 1
-            # if token in data is comma and token in corrected is not, add point to missing
-            elif data_token.text == ',' and corrected_token.text != ',':
+            # if token in sentences is comma and token in corrected is not, add point to missing
+            elif sentences_token.text == ',' and corrected_token.text != ',':
                 points["missing"] += 1
                 corrected_pos -= 1
-            # if token in data is not comma and token in corrected is, add point to incorrect
-            elif data_token.text != ',' and corrected_token.text == ',':
+            # if token in sentences is not comma and token in corrected is, add point to incorrect
+            elif sentences_token.text != ',' and corrected_token.text == ',':
                 points["incorrect"] += 1
-                data_pos -= 1
+                sentences_pos -= 1
 
             # increment both indexes
-            data_pos += 1
+            sentences_pos += 1
             corrected_pos += 1
 
         return points
+    
+    def _rate(self, corrected_docs: Language, sentences_docs: Language) -> dict[str, int]:
+        """
+        Rates correctness of corrected sentences.
+        """
 
-    def test(self) -> dict[str, int]:
-        # transform sentence to sentence without commas for testing purposes
-        no_commas_sentence = self._data.replace(",", "")
+        points: dict[str, int] = {"correct": 0, "incorrect": 0, "missing": 0}
+
+        for corrected_doc, sentences_doc in zip(corrected_docs, sentences_docs):
+            result = self._rate_sentence(corrected_doc, sentences_doc)
+            points["correct"] += result["correct"]
+            points["incorrect"] += result["incorrect"]
+            points["missing"] += result["missing"]
+
+        return points
+
+    def run(self) -> dict[str, int]:
+        # transform sentences to sentences without commas
+        no_commas_sentences: list[str] = [sentence.replace(',', '') for sentence in self._sentences]
 
         # set sentence and sentence_doc in corrector
-        self._corrector.sentence = no_commas_sentence
-        self._corrector.sentence_doc = self._corrector.create_doc(
-            no_commas_sentence)
+        self._corrector.sentences = no_commas_sentences
 
         # correct sentence and get result
-        corrected_sentence: str = self._corrector.correct()
+        corrected_sentences: str = self._corrector.run()
 
-        corrected_doc: Language = self._corrector.create_doc(
-            corrected_sentence)
-        data_doc = self._corrector.create_doc(self._data)
+        corrected_docs: Language = self._corrector.create_docs(
+            corrected_sentences)
+        sentences_doc = self._corrector.create_docs(self._sentences)
 
         # return rated result in format {'correct': int, 'incorrect': int, 'missing': int}
-        return self._rate(corrected_doc, data_doc)
+        return self._rate(corrected_docs, sentences_doc)
