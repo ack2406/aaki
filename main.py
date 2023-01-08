f6eff7f6de1359e643841bdeb7827d39862003fc
ack2406 <ack9222@gmail.com>
2023-01-08 01:02:57 +0100

    update main to test multi comma tester

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 from comma_corrector.multi_comma_corrector import MultiCommaCorrector
 from comma_corrector.comma_tester import CommaTester
+from comma_corrector.multi_comma_tester import MultiCommaTester
+
 
 def main():
     """Main function."""
+
+    tester = MultiCommaTester()
+
+    # get sentences from file resources/wyka-rzecz-wyobrazni.txt
+    with open("resources/wyka-rzecz-wyobrazni.txt", "r", encoding="utf-8") as file:
+        sentences = file.readlines()
     
 
-    corrector = MultiCommaCorrector()
+    tester.data = sentences
 
-    # daj mi polskie zdania
-    sentences = [
-        "To jest zdanie bez przecinka ale za to z głową.",
-        "Muchy, biedronki oraz owady nie są zwierzętami ale są roślinami.",
-        "Patryk poszedł do sklepu i spotkał się z kolegą, który mówił, że nie ma pieniędzy.",
-        "Patryk ma dobre serce, lecz nie jest szczęśliwy.",
-        "Patryk powiedział że nie ma pieniędzy, a jego kolega się zdenerwował",
-        "Patryk i kolega chcieli walczyć o pieniądze lecz policjant ich rozproszył.",
-        "Kolega Patryka gonił go, ponieważ Patryk nie chciał mu oddać pieniędzy.",
-        "Nikt już nie chce Patryka ponieważ on jest zły.",
-    ]
+    points = tester.test()
 
-    corrector.sentences = sentences
-    corrector.sentences_doc = corrector.create_docs(corrector.sentences)
-
-    corrected_sentences = corrector.correct()
-
-    for sentence in corrected_sentences:
-        print(sentence)
-
-
-
+    print(points)
 
 
 if __name__ == "__main__":
