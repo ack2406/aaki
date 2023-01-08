d8b7186418f2aa96f9ed8145df610fabbb8db213
ack2406 <ack9222@gmail.com>
2023-01-08 02:05:32 +0100

    update main to test corrector and tester

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-from comma_corrector.multi_comma_corrector import MultiCommaCorrector
 from comma_corrector.comma_tester import CommaTester
-from comma_corrector.multi_comma_tester import MultiCommaTester
+from comma_corrector.comma_corrector import CommaCorrector
 
 
 def main():
     """Main function."""
 
-    tester = MultiCommaTester()
-
     # get sentences from file resources/wyka-rzecz-wyobrazni.txt
     with open("resources/wyka-rzecz-wyobrazni.txt", "r", encoding="utf-8") as file:
-        sentences = file.readlines()
-    
+        sentences = [line.strip() for line in file.readlines()]
 
-    tester.data = sentences
+    corrector = CommaCorrector(sentences)
+    tester = CommaTester(sentences)
 
-    points = tester.test()
-
-    print(points)
+    print(corrector.run()[:10])
+    print(tester.run())
 
 
 if __name__ == "__main__":
